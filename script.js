90b5dba88d322125bc7128b5c9dee0b8635fc2b1
Vikram pratap <122038154+vikrampratap2002@users.noreply.github.com>
2026-03-26 21:38:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ cubes.forEach(cube => {
 
     cube.style.position = "absolute";
     cube.style.zIndex = "1000";
-    cube.style.cursor = "grabbing";
   });
 });
 
@@ -27,7 +26,6 @@ document.addEventListener("mousemove", (e) => {
   let x = e.clientX - containerRect.left - offsetX;
   let y = e.clientY - containerRect.top - offsetY;
 
-  // Boundary control
   x = Math.max(0, Math.min(x, container.clientWidth - activeCube.offsetWidth));
   y = Math.max(0, Math.min(y, container.clientHeight - activeCube.offsetHeight));
 
@@ -36,29 +34,5 @@ document.addEventListener("mousemove", (e) => {
 });
 
 document.addEventListener("mouseup", () => {
-  if (activeCube) {
-    activeCube.style.cursor = "grab";
-  }
   activeCube = null;
-});
-
-describe("Drag Test", () => {
-
-  beforeEach(() => {
-    cy.visit("http://localhost:3000");
-  });
-
-  it("should drag cube", () => {
-
-    cy.get('.items').should('be.visible');
-
-    cy.get('.cube').first()
-      .trigger('mousedown', { which: 1, clientX: 100, clientY: 100 })
-      .trigger('mousemove', { clientX: 250, clientY: 250 })
-      .trigger('mouseup');
-
-    cy.get('.cube').first()
-      .should('have.css', 'position', 'absolute');
-  });
-
 });
